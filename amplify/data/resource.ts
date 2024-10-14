c9251ffc9e0fe2acc722eadf0001fc22a3ec86c0
Alex Canady <mcc9513@gmail.com>
2024-10-14 15:52:58 -0400

    bug

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,29 +1,12 @@
-import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
+import { defineAuth } from "@aws-amplify/backend";
 
-const schema = a.schema({
-  BedrockResponse: a.customType({
-    body: a.string(),
-    error: a.string(),
-  }),
-
-  askBedrock: a
-    .query()
-    .arguments({ ingredients: a.string().array() })
-    .returns(a.ref("BedrockResponse"))
-    .authorization((allow) => [allow.authenticated()])
-    .handler(
-      a.handler.custom({ entry: "./bedrock.js", dataSource: "bedrockDS" })
-    ),
-});
-
-export type Schema = ClientSchema<typeof schema>;
-
-export const data = defineData({
-  schema,
-  authorizationModes: {
-    defaultAuthorizationMode: "apiKey",
-    apiKeyAuthorizationMode: {
-      expiresInDays: 30,
+export const auth = defineAuth({
+  loginWith: {
+    email: {
+      verificationEmailStyle: "CODE",
+      verificationEmailSubject: "Welcome to the AI-Powered Recipe Generator!",
+      verificationEmailBody: (createCode) =>
+        `Use this code to confirm your account: ${createCode()}`,
     },
   },
 });
